4d514c9d260618be74118e6f34401751ac50a60d
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2022-02-22 13:42:01 +0300

    финал

--- delegate_event/Final.cs
+++ delegate_event/Final.cs
@@ -9,17 +9,42 @@ namespace delegate_event
     class Final
     {
         public int Const { get; } = 0;  // точка старта
-        public int Temp { get; } = 1;
-        public int temp;
+      
+        public int temp,temp1;
         public Final()
         {
-            ICar[] a =
-              {
-                new Passenger_car(Const),
-                new Sports_car(Const),
-                new Truck(Const),
-                new Bus(Const)
-            };
+            var one = new Passenger_car(Const);
+            var two = new Sports_car(Const);
+            var thee = new Truck(Const);
+            var four = new Bus(Const);
+
+            ICar[] a = { one, two, thee,four };
+            for (int i = 0; i < a.Length; i++)
+            {
+                //Array.Sort( (Array) a[i].casd());
+                //if ( a[i].Count > temp)                                к сожалению сортировку не получилось организовать не какими методами 
+
+                //{
+                //    temp = a[i].Count;
+                   
+                //    temp1 = temp;
+                //    Console.WriteLine($" {i+1} =  {a[i].Count} {a[i].ToString()}");
+                //}
+          
+
+            }
+
+            //foreach (ICar  aa in a)
+            //{
+            //    Console.WriteLine($"{aa.Count}");
+            //}
+            ////ICar[] a =
+            ////  {
+            ////    new Passenger_car(Const),
+            ////    new Sports_car(Const),
+            ////    new Truck(Const),
+            ////    new Bus(Const)
+            ////};
 
 
 
@@ -28,19 +53,7 @@ namespace delegate_event
 
                 Console.WriteLine(asa);
             }
-            //for (int i = 0; i < a.Length; i++)
-            // {
-            //     if (a[i].casd() > temp)
-            //     {
-
-            //     }
-            // }
-
-            //foreach (ICar asa in saa)
-            //{
-
-            //    Console.WriteLine(asa);
-            //}
+           
 
         }
 
